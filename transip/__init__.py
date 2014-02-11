2647ea1aa03949a6ccb577ceb4b0f2fc82de60eb
Matthias Hogerheijde <matthias.hogerheijde@goabout.com>
2014-02-11 19:40:13 +0100

    Don't include transip.client.Client in __init__.py

--- transip/__init__.py
+++ transip/__init__.py
@@ -1,9 +1,9 @@
 """
 	TransIP API Connector
-	
+
 	A python implementation for usage of the API for TransIP.
 
-	For usage of the API itself, please see https://www.transip.eu/transip/api/	
+	For usage of the API itself, please see https://www.transip.eu/transip/api/
 """
 
 __author__ = "Go About"
@@ -11,6 +11,3 @@ __email__ = "tech@goabout.com"
 __version__ = "0.1.0-dev"
 __copyright__ = "Copyright (C) 2013 Go About B.V."
 __license__ = "MIT License"
-
-
-from transip.client import Client
